4a203eddaafa6b8086b8162b2e65b5bbb05f10c2
Stuart Langridge <sil@kryogenix.org>
2014-12-15 11:48:04 +0000

    my first commit

---
+++ index.js
@@ -0,0 +1,200 @@
+var express = require('express'),
+    app = exports.app = express(),
+    bodyParser = require('body-parser'),
+    pg = require('pg'),
+    exphbs  = require('express-handlebars'),
+    async = require('async'),
+    fs = require('fs'),
+    RSA = require('node-rsa'),
+    request = require('request');
+
+// load keys
+var key = new RSA();
+key.importKey(fs.readFileSync('private.key'));
+var pubkey = new RSA();
+pubkey.importKey(fs.readFileSync('public.key'));
+
+// Create database contents table on startup
+exports.dbStartup = function(dburl) {
+    pg.connect(dburl, function(err, client, returnClientToPool) {
+        if (err) {
+            throw new Error(err);
+        }
+        var sqls = [
+            'create table if not exists codes ' +
+                '(id serial primary key, pushtoken varchar, code varchar, ' +
+                'created timestamp DEFAULT now() NOT NULL)',
+            'CREATE OR REPLACE FUNCTION delete_old_rows() RETURNS trigger ' +
+                'LANGUAGE plpgsql AS $$ BEGIN ' +
+                "DELETE FROM codes WHERE created < NOW() - INTERVAL '15 minutes';" +
+                'RETURN NEW; END; $$;',
+            'DROP TRIGGER IF EXISTS old_rows_gc ON codes;',
+            'CREATE TRIGGER old_rows_gc AFTER INSERT ON codes ' +
+                'EXECUTE PROCEDURE delete_old_rows();'
+        ];
+        async.eachSeries(sqls, function(sql, cb) {
+            client.query(sql, function(err, result) {
+                returnClientToPool();
+                cb(err);
+            });
+        }, function(err) {
+            if (err) {
+                throw new Error(err);
+            }
+        });
+    });
+};
+
+app.set('dburl', process.env.DATABASE_URL);
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+function sendPushNotification(token, content, done) {
+    console.log("requesting with token", token);
+    request.post({
+        url: "https://push.ubuntu.com/notify",
+        json: true,
+        body: {
+            appid: "org.kryogenix.caxton_caxton",
+            expire_on: "2015-12-08T14:48:00.000Z",
+            token: token,
+            data: {
+                message: content,
+                notification: {
+                    card: {
+                        summary: "yes",
+                        body: "hello",
+                        popup: true,
+                        persist: true
+                    },
+                    sound: "buzz.mp3",
+                    tag: "foo",
+                    vibrate: {
+                        duration: 200,
+                        pattern: (200, 100),
+                        repeat: 2
+                    },
+                    "emblem-counter": {
+                        count: 12,
+                        visible: true
+                    }
+                }
+            }
+        }
+    }, function(err, resp) {
+        console.log("request done", resp.body);
+        done(err);
+    });
+}
+
+function unwrapPassedToken(enc) {
+    // the token we receive should actually be encrypted JSON {token: actualtoken}
+    var dec, pushtoken;
+    dec = key.decrypt(enc, 'utf8');
+    dec = JSON.parse(dec);
+    if (!dec.token) { throw new Error("No token entry in JSON: " + dec); }
+    return dec.token;
+}
+
+app.post('/api/getcode', function(req, res) {
+    if (!req.body || !req.body.pushtoken) {
+        console.log("Error getting code: incomplete request: ", req.body);
+        return res.status(400).json({error: "Incomplete request"});
+    }
+    // Create a random easy-to-type code
+    var possible = "abcdefghijklmnopqrstuvwxyz", code = "";
+
+    for(var i=0; i < 5; i++) {
+        code += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    pg.connect(app.get('dburl'), function(err, client, returnClientToPool) {
+        if (err) {
+            console.log("Error getting code: connection: ", err);
+            return res.status(500).json({error: "Server problem"});
+        }
+        client.query("insert into codes (pushtoken, code) values ($1::varchar, $2::varchar)", 
+            [req.body.pushtoken, code],
+            function(err, result) {
+            returnClientToPool();
+            if (err) {
+                console.log("Error getting code: insert: ", err);
+                return res.status(500).json({error: "Server problem"});
+            }
+            console.log("Returning a code to client", code);
+            res.json({code: code});
+        });
+    });
+});
+
+app.post('/api/gettoken', function(req, res) {
+    if (!req.body || !req.body.code) {
+        return res.status(400).json({error: "Incomplete request"});
+    }
+    pg.connect(app.get('dburl'), function(err, client, returnClientToPool) {
+        if (err) {
+            console.log("Error getting code: connection: ", err);
+            return res.status(500).json({error: "Server problem"});
+        }
+        client.query("select pushtoken from codes where code = $1::varchar", 
+            [req.body.code],
+            function(err, result) {
+            returnClientToPool();
+            if (err) {
+                console.log("Error getting code: insert: ", err);
+                return res.status(500).json({error: "Server problem"});
+            }
+            if (result.rows.length === 0) {
+                return res.status(404).json({error: "no such code"});
+            }
+            // encrypt token with public key before sending it to client
+            var enctoken = pubkey.encrypt(JSON.stringify({token:result.rows[0].pushtoken}), "base64");
+            res.json({token: enctoken});
+            // and remove token from DB
+            client.query("delete from codes where code = $1::varchar", [req.body.code], function(err, result) {
+                returnClientToPool();
+            });
+        });
+    });
+});
+
+app.post('/api/send', function(req, res) {
+    if (!req.body || !req.body.token || !req.body.url) {
+        console.log("Body missing parameters", req.body);
+        return res.status(400).json({error: "Incomplete request"});
+    }
+    var token;
+    try {
+        token = unwrapPassedToken(req.body.token);
+    } catch(e) {
+        console.log("Passed invalid token in send request", e);
+        return res.status(400).json({error: "Invalid token"});
+    }
+    sendPushNotification(token, {url:req.body.url}, function(err) {
+        if (err) {
+            console.log("Error sending push notification", e);
+            return res.status(500).json({error: "Push notification failed"});
+        }
+        res.json({ok: "Ok"});
+    });
+});
+
+if (require.main == module) {
+    exports.dbStartup(app.get('dburl'));
+    var server = app.listen(app.get('port'), function () {
+      var host = server.address().address;
+      var port = server.address().port;
+      console.log('Caxton server listening at http://%s:%s', host, port);
+    });
+}
---
+++ test/test-api.js
@@ -0,0 +1,158 @@
+var request = require('supertest'),
+    pg = require('pg'),
+    rewire = require('rewire'),
+    server = rewire('../index'),
+    should = require('should'),
+    RSA = require('node-rsa'),
+    fs = require("fs");
+
+describe('API',function(){
+
+    var key = new RSA();
+    key.importKey(fs.readFileSync('private.key'));
+    // strictly can get this from the private key but it tests the pubkey file is valid
+    var pubkey = new RSA();
+    pubkey.importKey(fs.readFileSync('public.key'));
+
+    before(function(done) {
+        server.app.set('dburl', process.env.DATABASE_URL);
+        server.dbStartup(process.env.DATABASE_URL);
+        done();
+    });
+
+    it('should fail to get a code without parameters', function(done) {
+        request(server.app).post("/api/getcode")
+            .expect(400, done);
+    });
+    it('should work with parameters', function(done) {
+        var pushtoken = "push" + Math.random();
+        request(server.app).post("/api/getcode")
+            .type('form')
+            .send({ pushtoken: pushtoken })
+            .expect(200)
+            .end(function(err, res) {
+                should.not.exist(err);
+                res.body.should.have.property('code');
+                // now confirm that token is in the database
+                pg.connect(server.app.get('dburl'), function(err, client, returnClientToPool) {
+                    should.not.exist(err);
+                    client.query("select code from codes where pushtoken = $1::varchar", 
+                    [pushtoken],
+                    function(err, results) {
+                        should.not.exist(err);
+                        returnClientToPool();
+                        if (err) { throw new Error(err); }
+                        results.rows.length.should.equal(1);
+                        results.rows[0].should.have.property('code');
+                        results.rows[0].code.should.equal(res.body.code);
+                        done();
+                });
+            });
+        });
+    });
+    it('should fail to get a token without parameters', function(done) {
+        request(server.app).post("/api/gettoken")
+            .expect(400, done);
+    });
+    it("should complain when passed an invalid code", function(done) {
+        request(server.app).post("/api/gettoken")
+            .type("form")
+            .send({code: "no sirree"})
+            .expect(404, done);
+    });
+    it("should get a token when passed a correct code", function(done) {
+        var pushtoken = "push" + Math.random();
+        request(server.app).post("/api/getcode")
+            .type('form')
+            .send({ pushtoken: pushtoken })
+            .expect(200)
+            .end(function(err, res) {
+                should.not.exist(err);
+                request(server.app).post("/api/gettoken")
+                    .type("form")
+                    .send({code: res.body.code})
+                    .expect(200)
+                    .end(function(err, res) {
+                        should.not.exist(err);
+                        res.should.have.property("body");
+                        res.body.should.have.property("token");
+                        var dec = JSON.parse(key.decrypt(res.body.token, "utf8"));
+                        dec.should.have.property("token");
+                        dec.token.should.equal(pushtoken);
+                        done();
+                });
+            });
+    });
+    it("should fail sends with no parameters", function(done) {
+        request(server.app).post("/api/send")
+            .expect(400, done);
+    });
+    it("should fail sends with an invalid token", function(done) {
+        request(server.app).post("/api/send")
+            .type("form")
+            .send({token: "no sirree", url: "no"})
+            .expect(400, done);
+    });
+    it("should fail sends with no url", function(done) {
+        request(server.app).post("/api/send")
+            .type("form")
+            .send({token: pubkey.encrypt(JSON.stringify({token:"testtoken"}), "base64")})
+            .expect(400, done);
+    });
+    it("should make a request when given a valid token", function(done) {
+        var passedtoken = "testtoken",
+            passedurl = "http://example.com",
+            called = false;
+        server.__set__("sendPushNotification", function(token, content, cb) {
+            token.should.equal(passedtoken);
+            content.should.have.property("url");
+            content.url.should.equal(passedurl);
+            called = true;
+            cb();
+        });
+        request(server.app).post("/api/send")
+            .type("form")
+            .send({token: pubkey.encrypt(JSON.stringify({token:passedtoken}), "base64")})
+            .send({url: passedurl})
+            .expect(200, function(err) {
+                called.should.equal(true);
+                done();
+            });
+    });
+    it("should get a token and use it to send correctly", function(done) {
+        var pushtoken = "push" + Math.random(),
+            passedurl = "http://example.com",
+            sendtoken,
+            called = false;
+        server.__set__("sendPushNotification", function(token, content, cb) {
+            content.should.have.property("url");
+            content.url.should.equal(passedurl);
+            token.should.equal(pushtoken);
+            called = true;
+            cb();
+        });
+        request(server.app).post("/api/getcode")
+            .type('form')
+            .send({ pushtoken: pushtoken })
+            .expect(200)
+            .end(function(err, res) {
+                should.not.exist(err);
+                request(server.app).post("/api/gettoken")
+                    .type("form")
+                    .send({code: res.body.code})
+                    .expect(200)
+                    .end(function(err, res) {
+                        should.not.exist(err);
+                        sendtoken = res.body.token;
+                        request(server.app).post("/api/send")
+                            .type("form")
+                            .send({token: sendtoken, url: passedurl})
+                            .expect(200, function(err) {
+                                called.should.equal(true);
+                                done();
+                        });
+                });
+            });
+
+    });
+});
