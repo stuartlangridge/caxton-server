54fe0a7626220b2885aac3cd85eafd6e98cc0c61
Stuart Langridge <sil@kryogenix.org>
2014-12-20 14:20:01 +0000

    dump incoming ifttt request

--- index.js
+++ index.js
@@ -108,6 +108,10 @@ var xroute = xrpc.route({
 
 app.post('/xmlrpc.php', function(req, res, next) {
     res.contentType('text/xml'); // without this, ifttt doesn't like us
+    console.log(req);
+    process.nextTick(function() {
+        visitor.event("Incoming XMLRPC", "unspecified").send();
+    });
     return xroute(req, res, next);
 });
 
